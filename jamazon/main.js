7cd04192c83330fc377691943eabc2a3d22cba95
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-06-05 11:12:16 -0700

    Adjusted bootstrap css classes and custome css, along with adding fontawesome icons and added more items. #1

--- jamazon/main.js
+++ jamazon/main.js
@@ -83,6 +83,46 @@ var app = {
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         origin: 'Japan',
         imageUrl: 'https://goo.gl/gJYUxz'
+      },
+      {
+        itemId: 4,
+        name: 'Hi-Hats',
+        brand: 'Paiste',
+        price: 400,
+        description: 'A set of hats for people who spare no expense.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Switzerland',
+        imageUrl: 'https://goo.gl/LV9l8O'
+      },
+      {
+        itemId: 5,
+        name: 'Giant Step',
+        brand: 'Sonor',
+        price: 599,
+        description: 'A pedal for metal drummers with only one foot.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Germany',
+        imageUrl: 'https://goo.gl/zsFo1q'
+      },
+      {
+        itemId: 3,
+        name: 'Djembe',
+        brand: 'Remo',
+        price: 49.99,
+        description: 'A hand drum for people with dreadlocks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'West Africa',
+        imageUrl: 'https://goo.gl/iKtU8x'
+      },
+      {
+        itemId: 1,
+        brand: 'Ludwig',
+        name: 'Black Beauty',
+        price: 749.99,
+        description: 'A snare drum for people with too many drumsticks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'United States',
+        imageUrl: 'https://goo.gl/ubY0QA'
       }
     ]
   },
@@ -100,7 +140,7 @@ function render(item) {
   let $cardText = document.createElement('p')
   let $cardBtn = document.createElement('a')
 
-  $card.classList.add('card', 'col-xl-3', 'col-lg-4', 'col-sm-6', 'col-12')
+  $card.classList.add('card', 'col-xl-2', 'col-lg-4', 'col-sm-6', 'col-12')
   $cardImg.classList.add('card-img-top')
   $cardImg.setAttribute('src', item.imageUrl)
   $cardImg.setAttribute('alt', 'Card image cap')
@@ -125,15 +165,18 @@ function render(item) {
 function renderCatalog(catalog) {
   let $container = document.createElement('div')
   let $headCont = document.createElement('h2')
+  let $headLogo = document.createElement('i')
   let $headRow = document.createElement('div')
   let $itemRow = document.createElement('div')
 
-  $headCont.textContent = 'Sal\'s Generic Online Music Retailer'
-  $headCont.classList.add('col', 'd-flex', 'justify-content-center')
+  $headCont.textContent = 'jamazon'
+  $headCont.classList.add('col', 'd-flex', 'justify-content-left')
   $container.classList.add('container-fluid')
+  $headLogo.classList.add('fas', 'fa-headphones')
   $headRow.classList.add('row')
   $itemRow.classList.add('row')
 
+  $headCont.appendChild($headLogo)
   $headRow.appendChild($headCont)
   $container.appendChild($headRow)
   $container.appendChild($itemRow)
