cbe2f37b4041eb3da1d12d6438c2bbe78f668999
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-06-07 14:10:21 -0700

    Enhanced detailTemplate() function to include button for returning to catalog. #4

--- jamazon/main.js
+++ jamazon/main.js
@@ -74,8 +74,7 @@ function detailTemplate(item) {
   const $imgCont = document.createElement('div')
   const $detailRow = document.createElement('div')
   const $cartBtn = document.createElement('button')
-
-  const $header = document.querySelector('.header')
+  const $returnBtn = document.createElement('button')
 
   $detailCont.classList.add('container')
   $detailInfo.classList.add('col', 'col-lg-9')
@@ -83,7 +82,8 @@ function detailTemplate(item) {
   $imgCont.classList.add('col', 'col-lg-3')
   $detailImg.classList.add('detail-img', 'd-flex', 'justify-content-start')
   $detailName.classList.add('d-flex', 'justify-content-start')
-  $cartBtn.classList.add('btn', 'btn-success', 'd-inline-flex', 'justify-content-around')
+  $cartBtn.classList.add('btn', 'btn-success')
+  $returnBtn.classList.add('btn', 'btn-secondary')
 
   $detailImg.setAttribute('src', item.imageUrl)
   $cartBtn.setAttribute('id', 'addcart')
@@ -94,10 +94,12 @@ function detailTemplate(item) {
   $detailDet.textContent = item.details
   $detailPrice.textContent = '$' + item.price
   $cartBtn.textContent = 'add to cart'
+  $returnBtn.textContent = 'continue shopping'
 
   $contAll.appendChild($detailRow)
   $detailCont.appendChild($detailRow)
   $detailPrice.appendChild($cartBtn)
+  $detailPrice.appendChild($returnBtn)
   $detailRow.appendChild($imgCont)
   $imgCont.appendChild($detailImg)
   $detailRow.appendChild($detailInfo)
